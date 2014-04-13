56fc370050d3014f48f9f000c54cf1c00845e355
Eric Gauderman <egauderman@gmail.com>
2014-04-13 06:31:23 -0700

    Initial commit of CompletedTasksPage

---
+++ homework_manager/homework_manager/CompletedTasksPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace homework_manager
+{
+	public partial class CompletedTasksPage : PhoneApplicationPage
+	{
+		public CompletedTasksPage()
+		{
+			InitializeComponent();
+
+			CompletedItemsItemsControl.DataContext = (App.Current as App).CompletedAssignmentItems;
+		}
+	}
+}
